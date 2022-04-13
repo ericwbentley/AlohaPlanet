e9e6d0341e2835136aa23ca28574d0723b87653e
Eric Bentley <ericwbentley@gmail.com>
2022-04-13 11:49:30 -0700

    Chapters 1 & 2

---
+++ Python/NoStarch_AutomateTheBoringStuff/Chapter 2/exitExample.py
@@ -0,0 +1,9 @@
+from ast import AsyncFunctionDef
+import sys
+
+while True:
+    print('Type exit to exit.')
+    response = input()
+    if response == 'exit':
+        sys.exit()
+    print('You typed ' + response + '.')
